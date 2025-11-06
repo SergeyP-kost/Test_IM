d91a549994180ce8b68e6fdb4d6d50b9430027e7
SAnatolP <sergeyp.sp@yandex.ru>
2025-11-06 19:43:46 +0300

    выбор предметов, исправление ошибок вывода вопросов

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.popup import Popup
-from kivymd.uix.textfield import MDTextField
 from kivymd.uix.label import MDLabel
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.button import MDFlatButton
@@ -21,7 +20,13 @@ Builder.load_file('screens/start_screen.kv')
 Builder.load_file('screens/main_screen.kv')
 Builder.load_file('screens/registration_screen.kv')
 
-COUNT_QUESTIONS = 2
+COUNT_QUESTIONS = 4
+SUBJECTS = {
+    "math": "Математика", 
+    "russian": "Русский язык", 
+    "history": "История",
+    "biology": "Биология"
+    }
 
 
 class StartScreen(MDScreen):
@@ -30,70 +35,124 @@ class StartScreen(MDScreen):
 
 
 class RegistrationScreen(MDScreen):
-    name = StringProperty("")
-    surname = StringProperty("")
-    class_name = StringProperty("")
-    subject = StringProperty("")
+    subject = None
+    subject_menu = None
 
     def register_user(self):
-        self.manager.current = 'main'
+        name_field = self.ids.name_field
+        surname_field = self.ids.surname_field
+        class_field = self.ids.class_field        
+
+        main_screen = self.manager.get_screen('main')
+        main_screen.registered_name = name_field.text
+        main_screen.registered_surname = surname_field.text
+        main_screen.registered_class = class_field.text
+        main_screen.registered_subject = self.subject
+
+        if not name_field.text:
+            name_field.error = True
+            name_field.helper_text = "Введите имя"
+        if not surname_field.text:
+            surname_field.error = True
+            surname_field.helper_text = "Введите фамилию"
+        if not class_field.text:
+            class_field.error = True
+            class_field.helper_text = "Введите класс, например 5а"
+        if self.subject is None:
+            self.ids.err_sub.error = True
+            self.ids.helper_text = "Выберите предмет"
+        if (name_field.text and surname_field.text and class_field.text and self.subject is not None):
+            name_field.error = False
+            surname_field.error = False
+            class_field.error = False
+            self.ids.err_sub.error = False
+
+            self.manager.current = 'main'
+
+
+    def menu_subjects(self):
+        menu_items = [
+            {
+                "text": name,
+                "on_release": lambda x=subject, name=name: self.menu_callback(x, name),
+            } for subject, name in SUBJECTS.items()
+        ]
+        self.subject_menu = MDDropdownMenu(
+            caller=self.ids.button, items=menu_items
+        )
+        self.subject_menu.open()
+
+    def menu_callback(self, text_item, name):
+        self.ids.button.text = name
+        self.subject = text_item
+
+        if self.subject:
+            self.subject_menu.dismiss()
 
 
 class MainScreen(MDScreen):
+
     grid_content = ObjectProperty(None)
-    spinners = []
-    questions = []
-    dropdown_menus = {}  # Словарь для хранения меню
+    user_answers = {}
+    questions = {}
+    dropdown_menus = {}
+    selected_questions = []
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    subject = None
+
+    def on_enter(self):
+        self.user_answers = {}
+        self.questions = {}
         self.dropdown_menus = {}
+        self.selected_questions = []
 
-    def on_kv_post(self, widget):
-        self.load_questions()
+        if self.registered_name and self.registered_subject:
+            self.ids.user.text = (
+                f"{self.registered_name} {self.registered_surname} {self.registered_class}"
+                )
+            self.ids.subject.text = SUBJECTS[self.registered_subject]
+            print(self.registered_subject)
+            self.subject = self.registered_subject
 
-    def load_questions(self):
         button = self.ids.questions_button
         button.text = 'Завершить'
 
         self.clear_grid()
-        self.spinners = {}
-        self.questions = []
 
-        file_path = 'example.txt'
+        file_path = f'questions/{self.subject}.txt'
+        print(file_path)
 
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
-                for line in file:
+                for i, line in enumerate(file):
                     parts = line.strip().split('|')
                     if len(parts) >= 3:
                         question = parts[0]
                         right_answer = parts[1]
                         answers = parts[1:]
 
-                        self.questions.append({
+                        self.questions[i] = {
                             "question": question,
                             "right_answer": right_answer,
                             "answers": answers
-                        })
+                        }
  
         except FileNotFoundError:
             self.show_file_not_found_popup(file_path)
 
-        selected_questions = select_random_items(self.questions, COUNT_QUESTIONS)
+        self.selected_questions = select_random_items(list(self.questions.keys()), COUNT_QUESTIONS)
 
-        for question_index, question in enumerate(selected_questions):
+        for question in self.selected_questions:
             self.grid_content.add_widget(
-                self.create_label(question["question"], halign='left')
+                self.create_label(self.questions[question]["question"], halign='left')
             )
             self.grid_content.add_widget(
                 self.create_dropdown_button(
-                    question["answers"], question_index, question["question"])
+                    self.questions[question]["answers"], question, self.questions[question]["question"])
             )
 
     def create_dropdown_button(self, options, question_index, question_text):
-        """Создание кнопки с выпадающим меню"""
-        # Создаем кнопку, которая будет открывать меню
+
         dropdown_button = MDFlatButton(
             text="Выберите ответ",
             size_hint_y=None,
@@ -106,23 +165,20 @@ class MainScreen(MDScreen):
 
         selected_answers = select_random_items(options, len(options))
         
-        # Создаем элементы меню
         menu_items = [
             {
                 "text": option,
-                "viewclass": "OneLineListItem",
+                # "viewclass": "OneLineListItem",
                 "on_release": lambda x=option, idx=question_index, btn=dropdown_button: self.on_answer_select(btn, x, idx),
             } for option in selected_answers
         ]
         
-        # Создаем меню
         menu = MDDropdownMenu(
             caller=dropdown_button,
             items=menu_items,
             max_height=300
         )
 
-        # Сохраняем ссылку на меню
         self.dropdown_menus[question_index] = menu
 
         dropdown_button.bind(on_release=lambda instance: menu.open())
@@ -130,18 +186,16 @@ class MainScreen(MDScreen):
         return dropdown_button
 
     def on_answer_select(self, button, answer, question_index):
-        """Обработчик выбора ответа"""
-        # Обновляем текст кнопки
         button.text = answer
-        self.spinners[question_index] = answer
+        self.user_answers[question_index] = answer
 
-        print(self.dropdown_menus)
+        if self.dropdown_menus[question_index]:
+            self.dropdown_menus[question_index].dismiss()
 
         if self.questions[question_index]["right_answer"] == answer:
             print("Верный ответ!!!")
         
         print(f"Вопрос {question_index+1}: Выбран ответ: {answer}")
-        print("Все ответы:", self.spinners)
 
     def show_file_not_found_popup(self, file_path):
         content = MDLabel(
@@ -171,17 +225,17 @@ class MainScreen(MDScreen):
         return label
 
     def accept_answers(self):
-        """Обработчик завершения теста"""
-        # Подсчет результатов
+
         correct_answers = 0
-        total_questions = len(self.questions)
         
-        for i in range(total_questions):
-            if i in self.spinners and self.spinners[i] == self.questions[i]["right_answer"]:
+        for i in self.selected_questions:
+            if i in self.user_answers and self.user_answers[i] == self.questions[i]["right_answer"]:
                 correct_answers += 1
-        
-        # Показываем результаты
-        self.show_results_popup(correct_answers, total_questions)
+
+        if len(self.selected_questions) < COUNT_QUESTIONS:
+            self.show_results_popup(correct_answers, len(self.selected_questions))  
+        else:          
+            self.show_results_popup(correct_answers, COUNT_QUESTIONS)
 
     def show_results_popup(self, correct, total):
         """Показать результаты теста"""
@@ -226,7 +280,7 @@ class MainScreen(MDScreen):
         popup.open()
 
     def clear_grid(self):
-        # Закрываем все открытые меню перед очисткой
+        
         for menu in self.dropdown_menus.values():
             if menu:
                 menu.dismiss()
@@ -239,17 +293,17 @@ class MainScreen(MDScreen):
 
 class TestIMApp(MDApp):
     questions = []
-        
-    def get_hex_color(self, hex_color):
-        return get_color_from_hex(hex_color)
 
     def build(self):
         # Цветовая схема
-        self.theme_cls.primary_palette = "Teal"
-        self.theme_cls.accent_palette = "Orange"
+        self.theme_cls.primary_palette = "Gray"
+        self.theme_cls.accent_palette = "Brown"
+        # 'Red', 'Pink', 'Purple', 'DeepPurple', 'Indigo', 'Blue', 
+        # 'LightBlue', 'Cyan', 'Teal', 'Green', 'LightGreen', 'Lime', 
+        # 'Yellow', 'Amber', 'Orange', 'DeepOrange', 'Brown', 'Gray', 'BlueGray'
         
         # Стиль темы
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         
         # Дополнительные настройки
         self.theme_cls.primary_hue = "500"
